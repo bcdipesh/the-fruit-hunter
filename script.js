b5f5b388f429705c0de4a4e0a949d5d4c87da480
bc-dipesh <bcdipeshwork@gmail.com>
2023-02-12 18:06:03 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// select DOM elements
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 
+//	Array of fruits that is used to search against user typed keyword and
+//	to display search result
 const FRUITS = [
 	'Apple',
 	'Apricot',
@@ -83,9 +86,17 @@ const FRUITS = [
 	'Yuzu',
 ];
 
+/**
+ * Takes a parameter that is used to search against the list of FRUITS
+ * and returns an array of matched results
+ *
+ * @param {string} str			The keyword typed by the user
+ * @returns {Array<string>}		An array of fruits that matched the user typed keyword
+ */
 function search(str) {
 	let results = [];
 
+	// for each fruit in the FRUITS array check and add it to results array if the parameter str matches any fruit
 	FRUITS.forEach((fruit) => {
 		if (fruit.toLowerCase().includes(str.toLowerCase())) {
 			results.push(fruit);
@@ -95,22 +106,39 @@ function search(str) {
 	return results;
 }
 
+/**
+ * Event handler that handles fruit search events whenever a keyboard button is released
+ *
+ * @param {Object} e	The event object passed by the event
+ */
 function searchHandler(e) {
 	const userInput = input.value;
+
+	// proceed only if userInput is not empty
 	if (userInput) {
 		const results = search(userInput);
 
+		// proceed only if the search results is not empty
 		if (results.length > 0) {
 			showSuggestions(results, userInput);
 		}
 	} else {
+		// clear the previous suggestion when the input field is blank
 		suggestions.innerHTML = '';
 	}
 }
 
+/**
+ * Displays the search results inside an unordered list
+ *
+ * @param {Array<string>} results	An array of fruits that matched the user typed keyword
+ * @param {string} inputVal			The keyword typed by the user inside the input field
+ */
 function showSuggestions(results, inputVal) {
+	// clear the previous suggestion
 	suggestions.innerHTML = '';
 
+	// for each fruit that is matched create an li element and append it to the ul element
 	results.forEach((result) => {
 		const li = document.createElement('li');
 		li.innerText = result;
@@ -119,6 +147,12 @@ function showSuggestions(results, inputVal) {
 	});
 }
 
+/**
+ * Event handler that handles click event on the li element.
+ * Replaces the input field with the fruit name contained inside the li element
+ *
+ * @param {Object} e	The event object passed by the event
+ */
 function useSuggestion(e) {
 	const selectedFruit = e.target.innerText;
 
